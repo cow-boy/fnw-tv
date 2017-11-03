098a46f651a5c72537e30a35a39a10dffa21fa77
cow-boy <hellohuxu@qq.com>
2017-11-03 19:03:06 +0800

    v0.22

---
+++ fnw-tv-web/src/main/resources/static/common/js/common.js
@@ -0,0 +1,61 @@
+//swiper
+window.onload = function() {
+    var mySwiper1 = new Swiper('#headerdh',{
+        freeMode : true,
+        slidesPerView : 'auto',
+        freeModeSticky : true ,
+    });
+}
+
+<!-- 底部悬浮 -->
+$(function () {
+    var e = $(".itop");
+    $(window).scroll(function () {
+        var t = $(document).scrollTop();
+        if (t > 300) {
+            e.fadeIn("slow")
+        } else {
+            e.fadeOut("slow")
+        }
+    });
+
+});
+$(".itop").click(function () {
+    $("html,body").animate({scrollTop: 0}, 500)
+})
+
+
+layui.use(['layer', 'form'], function () {
+    var layer = layui.layer
+        , form = layui.form;
+    layer.msg('欢迎享受视频乐趣',{icon: 1});
+});
+
+//判断是PC还是手持
+function IsPC(){
+    var userAgentInfo = navigator.userAgent;
+    var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }
+    }
+    return flag;
+}
+if (IsPC()) {
+    //显示效果
+    window.onload = function() {
+        var config = {
+            vx: 4,
+            vy: 4,
+            height: 2,
+            width: 2,
+            count: 200,
+            color: "121, 162, 185",
+            stroke: "130,255,255",
+            dist: 6000,
+            e_dist: 20000,
+            max_conn: 10
+        }
+        CanvasParticle(config);
+    }
+}
